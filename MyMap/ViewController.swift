abda728ca42761a8dd1cbb830655930cceda18c3
yukachilov <60588319+yukachilov@users.noreply.github.com>
2020-02-19 19:38:20 +0900

    Initial Commit

---
+++ MyMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMap
+//
+//  Created by 安部学 on 2020/02/19.
+//  Copyright © 2020 Manabu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
