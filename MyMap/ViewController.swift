5514f2cc283b500c09ad0f36c627ad091e707852
yukachilov <60588319+yukachilov@users.noreply.github.com>
2020-03-20 16:28:03 +0900

    gitHubに上げてみた

--- MyMap/ViewController.swift
+++ MyMap/ViewController.swift
@@ -7,14 +7,85 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        inputText.delegate = self
+        
     }
 
-
+    @IBOutlet weak var inputText: UITextField!
+   
+    
+    @IBOutlet weak var disMap: MKMapView!
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        
+        if let searchKey = textField.text {
+            print(searchKey)
+            
+            let geocoder = CLGeocoder()
+            
+            geocoder.geocodeAddressString(searchKey, completionHandler: { (placemarks,errpr) in
+                
+                if let unwrapPlacemarks = placemarks {
+                    
+                    if let firstPlacemark = unwrapPlacemarks.first {
+                        
+                        if let location = firstPlacemark.location {
+                            
+                            let targetCoordinate = location.coordinate
+                            
+                            print(targetCoordinate)
+                            
+                            let pin = MKPointAnnotation()
+                            
+                            pin.coordinate = targetCoordinate
+                            
+                            pin.title = searchKey
+                            
+                            self.disMap.addAnnotation(pin)
+                            
+                            self.disMap.region = MKCoordinateRegion(center: targetCoordinate, latitudinalMeters: 500.0, longitudinalMeters: 500.0)
+                            
+                            
+                            
+                        }
+                    }
+                }
+                
+                
+            })
+            
+        }
+        return true
+    }
+    
+    
+    
+    @IBAction func changeMapButton(_ sender: Any) {
+        if disMap.mapType == .standard {
+            disMap.mapType = .satellite
+        } else if disMap.mapType == .satellite {
+            disMap.mapType = .hybrid
+        } else if disMap.mapType == .hybrid {
+            disMap.mapType = .satelliteFlyover
+        } else if disMap.mapType == .satelliteFlyover {
+            disMap.mapType = .hybridFlyover
+        } else if disMap.mapType == .hybridFlyover {
+            disMap.mapType = .mutedStandard
+        } else {
+            disMap.mapType = .standard
+        }
+        
+    }
+    
 }
 
